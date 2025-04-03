7fe246fcfa838cad30f0c77cb0c85ea73511f3bd
Anshulkavi <a234nshul@gmail.com>
2025-04-03 21:54:06 +0530

    Fix refresh issue for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,10 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
+  build: {
+    outDir: "dist",
+    assetsDir: "assets", // 🛠 Fix assets path
+  },
   plugins: [
     react(),
     mode === 'development' && componentTagger(),
